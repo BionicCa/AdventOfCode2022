f2fe9de039ec3fa41c2e6ad201cf0c8c605fde8c
Simon Carstens <sc@flippler.de>
2022-12-06 13:45:17 +0100

    add code for solution day 1 puzzle

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,24 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun part1(input: List<List<Int>>): Int {
+        //return input.indices.maxBy { input[it].sum() } + 1
+        return input.maxOf { it.sum() }
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part2(input: List<List<Int>>): Int {
+        return input
+            .map { it.sum() }
+            .sortedDescending()
+            .take(3)
+            .sum()
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val testInput = readInputAsIntsList("Day01_test")
+    println(part1(testInput))
 
-    val input = readInput("Day01")
+
+    val input = readInputAsIntsList("Day01")
     println(part1(input))
+
     println(part2(input))
 }
--- src/Utils.kt
+++ src/Utils.kt
@@ -8,6 +8,20 @@ import java.security.MessageDigest
 fun readInput(name: String) = File("src", "$name.txt")
     .readLines()
 
+fun readInputAsIntsList(name: String) : List<List<Int>> {
+    val inputArray = readInput(name)
+    val map = inputArray.flatMapIndexed { index: Int, value: String ->
+        when {
+            index == 0 || index == inputArray.lastIndex -> listOf(index)
+            value.isEmpty() -> listOf(index - 1, index + 1)
+            else -> emptyList()
+        }
+    }
+    .windowed(size = 2, step = 2) { (from, to) -> inputArray.slice(from..to).map { it.toInt() }}
+
+    return map
+}
+
 /**
  * Converts string to md5 hash.
  */
